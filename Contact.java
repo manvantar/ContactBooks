8736f67a57d83eabefc6cfba594f7357c4dbdfd7
manvantar <manukvshetty@gmail.com>
2021-04-06 21:13:32 +0530

    Creating a Contact class with constructor

---
+++ Contact.java
@@ -0,0 +1,19 @@
+package com.addressbook;
+
+public class Contacts {
+    String firstname;
+    String lastname;
+    String city;
+    String state;
+    int zip;
+    String PhoneNumber;
+    String email;
+    public Contacts(String firstname,String lastname,String city,String state,int zip,String mob,String email) {
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.PhoneNumber=mob;
+        this.email=email;
+}
