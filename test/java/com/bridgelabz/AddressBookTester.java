0604e036421c1668afc4c25802fe1bc341b16106
manvantar <manukvshetty@gmail.com>
2021-04-26 13:17:00 +0530

    adding logic to check the duplicate available  then delete

---
+++ test/java/com/bridgelabz/AddressBookTester.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import com.bridgelabz.ContactBooks.AddressBookMain;
+import com.bridgelabz.ContactBooks.Book;
+import com.bridgelabz.ContactBooks.Contact;
+import org.junit.jupiter.api.Test;
+
+public class AddressBookTester {
+
+    @Test
+    public void givenContact_whenadded_shouldReturnTrue(){
+    Contact contact1=new Contact("Manu","Kv","Bengalur","Karnataka",560076,9663393660,"manukvshetty@gmail.com");
+    Book book=new Book();
+
+
+    }
+
+
+}
