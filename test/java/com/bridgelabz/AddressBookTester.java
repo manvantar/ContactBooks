06d9bd1c62e33718401b12ed29ade78fd1b935a1
manvantar <manukvshetty@gmail.com>
2021-04-26 16:47:05 +0530

    adding method to search city using stream API

--- test/java/com/bridgelabz/AddressBookTester.java
+++ test/java/com/bridgelabz/AddressBookTester.java
@@ -6,7 +6,15 @@ import com.bridgelabz.ContactBooks.Contact;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class AddressBookTester {
+    Contact contact1=new Contact("Manu","Kv","Bengaluru","Karnataka",
+            560076,"9663393660","manukvshetty@gmail.com");
+    Contact contact2=new Contact("Srinivar",null,"Bengaluru","Karnataka",
+            560076,"9663393660","srinivar@gmail.com");
+    Contact contact3=new Contact("inivar",null,"Mysuru","Karnataka",
+            560076,"9663393660","nivar@gmail.com");
 
 
     @Test
@@ -41,5 +49,18 @@ public class AddressBookTester {
         book.addNewContact(contact3);
         Assertions.assertEquals(contact3,book.checkContactExistsStream(contact3));
     }
+
+    @Test
+    public void givenCityToSearch_whenExists_shouldReturnContactList(){
+        Book book=new Book();
+        book.addNewContact(contact1);
+        book.addNewContact(contact3);
+        book.addNewContact(contact2);
+        List<Contact> cityList=book.checkContactByCityStream("Bengaluru");
+        List<Contact> cityList2=book.checkContactByCityStream("Mysuru");
+        Assertions.assertEquals(2,cityList.size());
+        Assertions.assertEquals(1,cityList2.size());
+    }
+
 
 }
